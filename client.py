69c21afea5e420c9b086631aa43e17a62c83ae97
Mata <103795245+RIBIOLLET-Mathieu@users.noreply.github.com>
2025-11-28 16:17:19 +0100

    Delete client.py

--- client.py
+++
@@ -1,9 +0,0 @@
-from zeep import Client
-
-client = Client("http://127.0.0.1:8000/?wsdl")
-
-resultat = client.service.calcul_temps_trajet(
-    distance_km=350, autonomie_km=150, temps_recharge_h=0.75
-)
-
-print(f"Temps total de trajet : {resultat:.2f} heures")
